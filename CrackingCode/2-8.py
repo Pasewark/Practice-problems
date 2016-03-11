1844d3a5e00000eda6468c769d9a6a9e08d00f4c
Pasewark <eric.pasewark@gmail.com>
2016-03-11 15:42:38 -0600

    Create 2-8.py

---
+++ CrackingCode/2-8.py
@@ -0,0 +1,18 @@
+class node:
+    nxt=None
+    def __init__(self,value):
+        self.value=value
+    def prnt(self):
+        current=self
+        while current!=None:
+            print current.value
+            current=current.nxt
+
+def loop(l):
+    nodes=set()
+    current=l
+    while current!=None:
+        if current in nodes: return True
+        nodes.add(current)
+        current=current.nxt
+    return False
